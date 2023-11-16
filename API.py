4b3a37420ac24ccb60824a4e79186dacab923e6e
unknown <noe.a57@outlook.com>
2023-11-16 13:22:26 -0600

    Se modifico el scrip API.py, para agregar doccentes, y se realizo un GET a Gabriel_Reyes

--- API.py
+++ API.py
@@ -6,31 +6,31 @@ app = Flask(__name__)
 def root ():
     return "bienbenidos"
 
-from alumnos import alumnos 
+from alumnos_docentes import alumnos_docentes 
 
-@app.route("/alumnos")
+@app.route("/alumnos_docentes")
 def getAlumnos():
-    return jsonify (alumnos)
+    return jsonify (alumnos_docentes)
 
 
 ## BUSCAR UN ALUMNO
-@app.route("/alumnos/<string:alumno_name>")
+@app.route("/alumnos_docentes/<string:alumno_name>")
 def getalumno(alumno_name):
-    alumnofound = [alumno for alumno in alumnos if alumno ["Nombre"] == alumno_name]
+    alumnofound = [alumno for alumno in alumnos_docentes if alumno ["Nombre"] == alumno_name]
     if (len(alumnofound) > 0):
-        return jsonify ({"alumno": alumnofound[0]})
+        return jsonify ({"alumno_docentes": alumnofound[0]})
 
 ## AGREGAR UN ALUMNO 
-@app.route("/alumnos", methods=["POST"])
+@app.route("/alumnos_docentes", methods=["POST"])
 def addalumno():
     alumno = request.json
-    alumnos.append(alumno)
-    return jsonify({"mensaje": "alumno agregado satisfactoriamente", "alumnos":alumnos})
+    alumnos_docentes.append(alumno)
+    return jsonify({"mensaje": "alumno agregado satisfactoriamente", "alumnos_docentes":alumnos_docentes})
 
 ## Actualizar UN ALUMNO
-@app.route("/alumnos/<string:alumno_name>", methods=["PATCH"])
+@app.route("/alumnos_docentes/<string:alumno_name>", methods=["PATCH"])
 def patchalumno(alumno_name):
-    alumno_found = [alumno for alumno in alumnos if alumno["Nombre"] == alumno_name]
+    alumno_found = [alumno for alumno in alumnos_docentes if alumno["Nombre"] == alumno_name]
     if len(alumno_found) > 0:
         alumno_actualizado = {}
         for key, value in request.json.items():
@@ -38,18 +38,18 @@ def patchalumno(alumno_name):
         alumno_found[0].update(alumno_actualizado)
         return jsonify({
             "mensaje": "Alumno Actualizado satisfactoriamente",
-            "alumno": alumno_found[0]
+            "alumno_docentes": alumno_found[0]
 })
     
 ## ELIMINAR UN ALUMNO
-@app.route("/alumnos/<string:alumno_name>", methods=["DELETE"])
+@app.route("/alumnos_docentes/<string:alumno_name>", methods=["DELETE"])
 def eliminalumno(alumno_name):
-    alumnosfound = [alumno for alumno in alumnos if alumno["Nombre"] == alumno_name]
+    alumnosfound = [alumno for alumno in alumnos_docentes if alumno["Nombre"] == alumno_name]
     if len(alumnosfound) > 0:
-        alumnos.remove(alumnosfound[0])
+        alumnos_docentes.remove(alumnosfound[0])
         return jsonify({
             "mensaje": "Alumno Eliminado",
-            "alumnos": alumnos
+            "alumnos_docentes": alumnos_docentes
 })
 
 if __name__ == "__main__":
---
+++ alumnos_docentes.py
@@ -0,0 +1,10 @@
+alumnos_docentes =[
+    {"Nombre": "Gabriel_Reyes",
+     "Especialidad": "Automatizacion",
+     "Numero_de_Empleado": 62431,
+     "Edificio": 4 
+     
+    }
+]
+
+
