62de66041e4dcabb1490c8cde5557eccedaf7ea8
unknown <noe.a57@outlook.com>
2023-11-16 14:03:49 -0600

    Se realizo el metodo DELETE, eliminado a la docente Patricia_Mora y al alumno Noe_Cardona

--- alumnos_docentes.py
+++ alumnos_docentes.py
@@ -4,18 +4,6 @@ alumnos_docentes =[
      "Numero_de_Empleado": 55669,
      "Edificio": 2 
      
-    },
-    {
-        "Nombre": "Patricia_Mora",
-        "Especialidad": "Optativa/BI",
-        "Numero_de_Eempleado": 77668,
-        "Edificio": 2
-    },
-    {
-        "Nombre": "Noe_Cardona",
-        "Especialida": "Redes",
-        "Matricula": 20045148,
-        "Grupo": "9IRICA"
     }
     
 ]
